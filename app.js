33a21709a9afa36e44f5004fd59808dada1d9801
Vani11aG310 <49530369+Vani11aG310@users.noreply.github.com>
2020-11-25 20:37:46 -0800

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ var helmet = require('helmet');
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://A01053172:Angvil_02@cluster0.1jqyl.mongodb.net/local_library?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://A01053172:Angvil_02@cluster0.1jqyl.mongodb.net/local_library?retryWrites=true'
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
